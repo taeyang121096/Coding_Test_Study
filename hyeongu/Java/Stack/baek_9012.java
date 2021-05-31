d273d03e7f96cbb0fc4a0fcabf8b6358f0877785
wancern <57997390+wancern@users.noreply.github.com>
2021-05-31 23:16:07 +0900

    Update baek_9012.java

--- hyeongu/Java/Stack/baek_9012.java
+++ hyeongu/Java/Stack/baek_9012.java
@@ -6,7 +6,7 @@ public class baek_9012 {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-
+		
 		int T = Integer.parseInt(br.readLine());
 		String command;
 
@@ -17,11 +17,12 @@ public class baek_9012 {
 
 			for (int i = 0; i < command.length(); i++) {
 				char c = command.charAt(i);
-
+				// ( 가 입력되면 스택에 push 
 				if(c=='(') {
 					stack.push("(");
 				}
 				else {
+					// )가 입력되었을때 스택이 비어있으면 false 스택이 차있으면 pop
 					if (stack.empty()) {
 						res = false;
 					} else {
@@ -31,6 +32,7 @@ public class baek_9012 {
 				if (!res)
 					break;
 			}
+			//스택이 비어있으면 YES 그렇지 않거나 앞에서 false값이 전달됐으면 NO
 			if (res) {
 				if (stack.empty()) {
 					sb.append("YES").append('\n');
