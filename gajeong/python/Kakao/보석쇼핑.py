6e9b5adf9dd50eec9d1275576fcb7f6c53df114f
gajeong <rlarkwjd127@gmail.com>
2021-06-23 21:56:07 +0900

    보석쇼핑

---
+++ gajeong/python/Kakao/보석쇼핑.py
@@ -0,0 +1,16 @@
+def solution(gems):
+    answer = []
+    data = set(gems)
+
+    print(data)
+
+    for i in range(len(data),len(gems)):
+      for j in range(0,len(gems)-i):
+        print(j)
+        print(set(gems[j:j+i]))
+        if set(gems[j:j+i+1]) == data:
+          return [j+1,j+i]
+    return answer
+
+gems = 		["ZZZ", "YYY", "NNNN", "YYY", "BBB"]
+print(solution(gems))
