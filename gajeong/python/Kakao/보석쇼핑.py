273b615ca787959177098765d44edb3b268c2c86
gajeong <rlarkwjd127@gmail.com>
2021-06-29 02:01:44 +0900

    파일정리

--- gajeong/python/Kakao/보석쇼핑.py
+++ gajeong/python/Kakao/보석쇼핑.py
@@ -1,24 +1,16 @@
-'''
-시간초과로 실패한 보석쇼핑 
-완전탐색을 활용한 특정 문자열 길이만큼 비교하는 방식
-데이터의 갯수가 최대 10000 이므로 시간적인 문제에서  
-너무 오래걸려 정상적인 해답 X 
-
-'''
-
 def solution(gems):
+    answer = []
     data = set(gems)
-    # 보석의 갯수부터, 전체 보석까지  
-    if len(data)==1:
-      return [1,1]
-    elif len(data)==len(gems):
-      return [1,len(gems)]
-    for i in range(len(data),len(gems)+1):
-      #0번째 인덱스부터 ~ 마지막인데스- 봐야하는보석의 갯수 
-      for j in range(0,len(gems)-i+1):
-        if set(gems[j:j+i]) == data:
+
+    print(data)
+
+    for i in range(len(data),len(gems)):
+      for j in range(0,len(gems)-i):
+        print(j)
+        print(set(gems[j:j+i]))
+        if set(gems[j:j+i+1]) == data:
           return [j+1,j+i]
+    return answer
 
-
-gems = 	["XYZ", "XYZ", "XYZ"]
+gems = ["ZZZ", "YYY", "NNNN", "YYY", "BBB"]
 print(solution(gems))
