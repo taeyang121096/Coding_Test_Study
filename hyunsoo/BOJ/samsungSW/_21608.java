42e7ef4bee872d75b02f04a2a6d8d9b2202ca726
KIM HYUN SOO <rlagustn9149@naver.com>
2021-10-21 18:01:32 +0900

    2021/10/21

--- hyunsoo/BOJ/samsungSW/_21608.java
+++ hyunsoo/BOJ/samsungSW/_21608.java
@@ -8,21 +8,35 @@ public class _21608 {
   static int[][] favor;
   static int[] dx={0,0,1,-1};
   static int[] dy={1,-1,0,0};
-  static List<Integer> list;
+  static List<node> list;
+  static int max;
+  static int answer=0;
 
   static class node{
     int x;
     int y;
-    public node(int x, int y){
+    int blank;
+    public node(int x, int y, int blank){
       this.x=x;
       this.y=y;
+      this.blank=blank;
     }
+    // public int compareTo(node o){
+    //   // if(o.blank == blank){
+    //   //   if(o.x==x){
+    //   //     return y-o.y;
+    //   //   }
+
+    //   //   return x-o.x;
+    //   // }
+    //   return blank - o.blank;
+    // }
   }
   public static void main(String[] args)throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     N= Integer.parseInt(br.readLine());
 
-    arr = new int[N+1][N+1];
+    arr = new int[N][N];
     favor = new int[N*N][5];
 
 
@@ -31,22 +45,132 @@ public class _21608 {
       for(int j=0; j<5; j++){
         favor[i][j]=Integer.parseInt(st.nextToken());
       }
+      findfavor(i); 
+      
+    //   for(int k=0; k<N; k++){
+    //   for(int j=0; j<N; j++){
+    //     System.out.print(arr[k][j]+" ");
+    //   }
+    //   System.out.println();
+    // }
+    // System.out.println();
+      
     }
+
+    // for(int i=0; i<N; i++){
+    //   for(int j=0; j<N; j++){
+    //     System.out.print(arr[i][j]+" ");
+    //   }
+    //   System.out.println();
+    // }
+
+    findanswer();
+    System.out.print(answer);
+
   }
-
-  public static void findBlack(int index){
-
-    for(int i=1; i<=N; i++){
-      for(int j=1; j<=N; j++){
-        for(int k=0; k<4; k++){
-          int tx = i+dx[i];
-          int ty = j+dy[i];
-          if(tx<0 || ty<0 || tx>N || ty>N) continue;
-
-          arr[tx][ty]==
+  public static void findanswer(){
+    
+    for(int i=0; i<N; i++){
+      for(int j=0; j<N; j++){
+        for(int k=0; k<N*N; k++){
+          if(arr[i][j]==favor[k][0]){
+            int count=0;  
+            for(int m=0; m<4; m++){
+              int tx= i+dx[m];
+              int ty =j+dy[m];
+              if(tx<0 || ty<0 || tx>=N || ty>=N) continue;
+              for(int l=1; l<=4; l++){
+                if(favor[k][l]==arr[tx][ty]) count++;
+              }  
+            }
+            switch(count){
+              case 0:
+                answer +=0;
+                break;
+              case 1:
+                answer +=1;
+                break;
+              case 2:
+                answer +=10;
+                break;
+              case 3:
+                answer +=100;
+                break;  
+              case 4:
+                answer +=1000;
+                break;
+            }
+          }
         }
       }
     }
   }
 
+ public static void findfavor(int index){
+    
+  list = new LinkedList<>();
+  max=0;
+  for(int i=0; i<N; i++){
+    for(int j=0; j<N; j++){  
+      
+      int count=0;
+      int blank=0;
+      if(index==8){
+        
+      }
+      if(arr[i][j]==0){
+        for(int k=0; k<4; k++){
+          int tx = i+dx[k];
+          int ty = j+dy[k];
+         
+          if(tx<0 || ty<0 || tx>=N || ty>=N) continue;
+          if(arr[tx][ty]!=0){  
+            for(int l=1; l<=4; l++){
+              
+              if(favor[index][l]==arr[tx][ty]) count++;
+            }  
+          }else{
+            blank++;
+          }
+        }
+      }
+      
+      if(count>max){
+        max=count;
+        list.clear();
+         if(index==8)
+         System.out.println(1+" ----"+ i+" "+j+" "+blank);
+        list.add(new node(i,j,blank));
+      }else if(count==max){
+        if(index==8)
+        System.out.println(2+" ----"+ i+" "+j+" "+blank);
+        list.add(new node(i,j,blank));
+      }
+    }
+  }
+
+  Collections.sort(list, new Comparator<node>(){
+    public int compare(node o1, node o2){
+      if(o2.blank==o1.blank){
+        if(o1.x==o2.x){
+          return o1.y-o2.y;
+        }
+        return o1.x-o2.x;
+      }
+      return o2.blank - o1.blank;
+    }
+  });
+
+  if(index==8){
+    System.out.println(list.size());
+    for(int i=0; i<list.size(); i++){
+      node tmp = list.get(i);
+      System.out.println(tmp.x+" "+tmp.y+" "+tmp.blank);
+      System.out.println(favor[index][0]);
+    }
+  } 
+  
+  node tmp = list.get(0);
+  arr[tmp.x][tmp.y]=favor[index][0];
+ }
 }
