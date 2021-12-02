4f006d7db717137738f38dcb710fc0a8398825c6
taeyang121096 <taeyang121096@naver.com>
2021-12-02 20:20:30 +0900

    20211202 algorithm

---
+++ taeyang/java/basic/Bigdiff.java
@@ -0,0 +1,24 @@
+package basic;
+import java.util.*;
+import java.io.*;
+public class Bigdiff {
+
+    public static void main(String[]args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr =new int[n+1];
+        int[] dp =new int[n+1];
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        for(int i =1;i<=n;i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+        dp[1] = arr[1];
+        for(int i = 2;i<=n;i++){
+            dp[i] = arr[i];
+            for(int j = 1;j<i;j++){
+                if(arr[j]<arr[i])
+                    dp[i] = Math.max(dp[i],dp[j]+arr[i]);
+            }
+        }
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+    }
+}
