050c197d6445b1c58d33f55a4f1686f6e44ba45b
taeyang121096 <taeyang121096@naver.com>
2021-12-08 21:09:54 +0900

    20211208 fix algorithm

--- taeyang/java/basic/Sum2.java
+++ taeyang/java/basic/Sum2.java
@@ -10,14 +10,15 @@ public class Sum2 {
         int[][] dp = new int[n+1][2];
         for(int i = 1;i<=n;i++)
             arr[i] = Integer.parseInt(st.nextToken());
-        dp[1][0] = arr[1];
-        dp[1][1] = 0;
+
+        dp[1][0] = 0;
+        dp[1][1] = arr[1];
+        int max = dp[1][1];
         for(int i = 2;i<n;i++) {
-            dp[i][0] = Math.max(dp[i - 1][0] + arr[i], arr[i]);
-            dp[i][1] = Math.max(dp[i - 1][1] + arr[i], dp[i-1][0]);
+            dp[i][0] = Math.max(dp[i-1][0]+arr[i],dp[i-1][1]);
+            dp[i][1] = Math.max(dp[i-1][1]+arr[i],arr[i]);
+            max = Math.max(dp[i][0],dp[i][1]);
         }
-        int max = 0;
-        max = Arrays.stream(dp).flatMapToInt(s->Arrays.stream(s)).max().getAsInt();
         System.out.println(max);
 
     }
