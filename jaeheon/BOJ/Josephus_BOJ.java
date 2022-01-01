b3f4b83e02281e8a59c4c421eec6b8a787cd0d79
Jaeheon Jeong <drdd1120@naver.com>
2022-01-01 21:38:37 +0900

    setlength 수정

--- jaeheon/BOJ/Josephus_BOJ.java
+++ jaeheon/BOJ/Josephus_BOJ.java
@@ -19,15 +19,15 @@ public class Josephus_BOJ {
 		sb.append("<");
 		
 		// 0보다 크게 된다면 poll이 마지막에 null을 반환
-		while(queue.size() > 1) {
+		while(!queue.isEmpty()) {
 			for(int i = 0; i < k-1; i++) {
 				int index = queue.poll(); // queue에 첫번째 값을 반환하고 제거, 비어있다면 null
 				queue.add(index); // 제거한 index를 queue에 추가 // ex) k = 2 : 123456 -> 23456 -> 234561 -> 34561 -> 345612
 			}
 			sb.append(queue.poll() + ", "); // 가장 내부에 있는 값을 반환하고 쉼표도 추가 -> 3
 		}
-		
-		sb.append(queue.poll() + ">"); // while문을 통과 후 마지막에 남아있는 값을 반환하고 '>' 추가
+		sb.setLength(sb.length() - 2); // 마지막 ", " 지우기
+		sb.append(">"); // while문을 통과 후 마지막에 남아있는 값을 반환하고 '>' 추가
 		System.out.print(sb);
 		
 	}
