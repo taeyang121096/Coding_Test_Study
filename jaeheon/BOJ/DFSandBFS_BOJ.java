868b71f4e2f0ad2e25c90f85a517023a7c8581bb
Jaeheon Jeong <drdd1120@naver.com>
2022-01-15 21:55:55 +0900

    20220115

---
+++ jaeheon/BOJ/DFSandBFS_BOJ.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+// 백준  dfs와 bfs    1260
+public class DFSandBFS_BOJ {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+	}
+
+}
