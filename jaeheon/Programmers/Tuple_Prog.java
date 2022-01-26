8587e99821643d3b14234e8cbbda84d0eba8fed3
Jaeheon Jeong <drdd1120@naver.com>
2022-01-27 01:59:36 +0900

    20220127

---
+++ jaeheon/Programmers/Tuple_Prog.java
@@ -0,0 +1,48 @@
+package Programmers;
+
+//양쪽 중괄호 제거(두개씩)
+//가운데 중괄호 제거
+//정렬
+//, 기준으로 나누고 하나씩 add
+// 넣어주려는 값이 이미 포함되어 있으면 pass
+import java.util.*;
+
+class Tuple_Prog {
+
+	public int[] solution(String s) {
+
+		// 중괄호 제거 -> input
+		s = s.substring(2, s.length() - 2).replace("},{", " ");
+		String[] input = s.split(" ");
+
+		// 길이 순으로 오름차순 정렬
+		Arrays.sort(input, (o1, o2) -> o1.length() > o2.length() ? 1 : -1);
+		System.out.println(Arrays.toString(input));
+
+		ArrayList<Integer> al = new ArrayList<>();
+
+		for (String tmp : input) {
+			String[] str = tmp.split(",");
+
+			for (int i = 0; i < str.length; i++) {
+				int num = Integer.valueOf(str[i]);
+
+				// 넣어주려는 값이 포함되어 있지 않으면 add
+				// 포함되어 있으면 continue
+				if (!al.contains(num)) {
+					al.add(num);
+				} else {
+					continue;
+				}
+			}
+		}
+
+		int[] answer = new int[al.size()];
+		
+		for (int i = 0; i < al.size(); i++) {
+			answer[i] = al.get(i);
+		}
+
+		return answer;
+	}
+}
