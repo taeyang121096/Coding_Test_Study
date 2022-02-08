f468eda0a97d4f4ccfbc19c0c47bf67f55750b0c
Jaeheon Jeong <drdd1120@naver.com>
2022-02-09 04:11:22 +0900

    20220209

---
+++ jaeheon/Programmers/BracketRotate_Prog.java
@@ -0,0 +1,49 @@
+package Programmers;
+
+import java.util.*;
+
+public class BracketRotate_Prog {
+	class Solution {
+		// s길이 만큼 넘기면 된다.
+//
+		public int solution(String s) {
+			int answer = 0;
+
+			Deque<Character> q = new ArrayDeque<>();
+			for (char c : s.toCharArray()) {
+				q.offer(c);
+			}
+			for (int i = 0; i < s.length(); i++) {
+				q.offer(q.pollFirst());
+				Stack<Character> stack = new Stack<>();
+				boolean flag = false;
+				for (char c : q) {
+					if (c == '(' || c == '{' || c == '[') {
+						stack.push(c);
+						continue;
+					} else {
+						if (stack.isEmpty()) {
+							flag = true;
+							break;
+						} else {
+							if (c == ')' && stack.peek() == '(')
+								stack.pop();
+							else if (c == ']' && stack.peek() == '[')
+								stack.pop();
+							else if (c == '}' && stack.peek() == '{')
+								stack.pop();
+							else {
+								flag = true;
+								break;
+							}
+						}
+					}
+				}
+				if (!flag && stack.isEmpty())
+					answer++;
+			}
+
+			return answer;
+		}
+	}
+}
