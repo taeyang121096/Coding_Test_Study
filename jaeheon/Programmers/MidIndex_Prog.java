b680db222344f90da7880a8d3bdbffb918f54d8e
Jaeheon Jeong <drdd1120@naver.com>
2022-02-19 11:05:17 +0900

    20220219

---
+++ jaeheon/Programmers/MidIndex_Prog.java
@@ -0,0 +1,15 @@
+package Programmers;
+
+class MidIndex_Prog {
+	public String solution(String s) {
+		String answer = "";
+
+		if (s.length() % 2 == 0) {
+			answer = s.substring(s.length() / 2 - 1, s.length() / 2 + 1);
+		} else {
+			answer = s.substring(s.length() / 2, s.length() / 2 + 1);
+		}
+
+		return answer;
+	}
+}
