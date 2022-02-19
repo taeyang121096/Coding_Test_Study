e714e48758a62f9d029eee0c96a6c5f4b3f7f428
Jaeheon Jeong <drdd1120@naver.com>
2022-02-19 11:27:33 +0900

    20220219

--- jaeheon/Programmers/HateSameNum_Prog.java
+++ jaeheon/Programmers/HateSameNum_Prog.java
@@ -1,5 +1,24 @@
 package Programmers;
 
-public class HateSameNum_Prog {
+import java.util.*;
 
+public class HateSameNum_Prog {
+	public int[] solution(int[] arr) {
+
+		ArrayList<Integer> list = new ArrayList<>();
+		int tmp = 10;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != tmp) {
+				list.add(arr[i]);
+				tmp = arr[i];
+			}
+		}
+
+		int[] answer = new int[list.size()];
+		for (int i = 0; i < list.size(); i++) {
+			answer[i] = list.get(i);
+		}
+
+		return answer;
+	}
 }
