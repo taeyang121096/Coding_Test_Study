4c8204404762fac2159f16d651d4aca403a3abf7
Jaeheon Jeong <drdd1120@naver.com>
2022-03-17 11:42:46 +0900

    20220317

---
+++ jaeheon/Programmers/N2Tiling_Prog.java
@@ -0,0 +1,17 @@
+package Programmers;
+
+class N2Tiling_Prog {
+	public int solution(int n) {
+
+		int[] dp = new int[n + 2];
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] = 2;
+
+		for (int i = 3; i <= n; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+		}
+
+		return dp[n];
+	}
+}
