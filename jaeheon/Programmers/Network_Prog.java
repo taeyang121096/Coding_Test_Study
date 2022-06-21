72d82b53a44437147ef909674048cb0559b06df1
Jaeheon Jeong <drdd1120@naver.com>
2022-06-22 04:01:23 +0900

    20220622

--- jaeheon/Programmers/Network_Prog.java
+++ jaeheon/Programmers/Network_Prog.java
@@ -1,28 +1,32 @@
 package Programmers;
 
+//n = 컴퓨터의 개수
+//computers 연결에 대한 정보가 담긴 2차원 배열
+//computers[i][j] = 1 -> i, j 연결
+
+
 class Network_Prog {
-    static boolean[] isChecked;
-    
-    public void dfs(int i, int[][] computers){
-        isChecked[i] = true;
-        
-        for(int j = 0; j < computers.length; j++){
-            if(isChecked[j] == false && computers[i][j] == 1){
-                dfs(j, computers);
-            }
-        }
-    }
-    
-    public int solution(int n, int[][] computers) {
-        int answer = 0;
-        isChecked = new boolean[n];
-        
-        for(int i = 0; i < n; i++){
-            if(isChecked[i] == false){
-                answer++;
-                dfs(i, computers);
-            }
-        }
-        return answer;
-    }
+ 
+ static void dfs(int[][] computer, int i, boolean[] visit){
+     visit[i] = true;
+     for(int j = 0; j < computer[0].length; j++){
+         if(!visit[j] && computer[i][j] == 1){
+             dfs(computer, j, visit);
+         }
+     }
+ }
+ 
+ public int solution(int n, int[][] computers) {
+     int answer = 0;
+     
+     boolean[] visit = new boolean[n];
+     for(int i = 0; i < n; i++){
+         if(!visit[i]){
+             dfs(computers, i, visit);
+             answer++;
+         }
+     }
+     
+     return answer;
+ }
 }
