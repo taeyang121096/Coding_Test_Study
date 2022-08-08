7c6dc58e50b608800bb3aaf5257824da5d5ee1a7
Jaeheon Jeong <drdd1120@naver.com>
2022-08-09 00:48:05 +0900

    20220809

--- jaeheon/BOJ/BlackJack_BOJ.java
+++ jaeheon/BOJ/BlackJack_BOJ.java
@@ -4,33 +4,39 @@ import java.io.*;
 import java.util.*;
 
 public class BlackJack_BOJ {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		
+
 		int[] num = new int[N];
 		st = new StringTokenizer(br.readLine());
-		
-		for(int i = 0; i < N; i++) {
+
+		for (int i = 0; i < N; i++) {
 			num[i] = Integer.parseInt(st.nextToken());
 		}
-		
+
 		int sum = 0;
 
-		for(int i = 0; i < N; i++) {
-			for(int j = i+1; j < N; j++) {
-				for(int k = j+1; k < N; k++) {
-					if(sum < num[i] + num[j] + num[k]){
-						if(M >= num[i] + num[j] + num[k]) {
+		for (int i = 0; i < N-2; i++) {
+			if (num[i] > M) {
+				continue;
+			}
+			for (int j = i + 1; j < N-1; j++) {
+				if(num[i] + num[j] > M) {
+					continue;
+				}
+				for (int k = j + 1; k < N; k++) {
+					if (sum < num[i] + num[j] + num[k]) {
+						if (M >= num[i] + num[j] + num[k]) {
 							sum = num[i] + num[j] + num[k];
 						}
 					}
 				}
 			}
 		}
-		
+
 		System.out.println(sum);
 	}
 }
