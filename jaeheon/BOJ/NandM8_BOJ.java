82f56b26562190f4f6924238967d83425264acf0
Jaeheon Jeong <drdd1120@naver.com>
2022-08-12 03:03:25 +0900

    20220812

---
+++ jaeheon/BOJ/NandM8_BOJ.java
@@ -0,0 +1,56 @@
+package BOJ;
+
+//백준 n과m(8) 15657
+
+import java.util.*;
+import java.io.*;
+
+public class NandM8_BOJ {
+	static int n, m;
+	static int[] arr;
+	static int[] answer;
+	static boolean[] visit;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		arr = new int[n];
+		visit = new boolean[n];
+		answer = new int[n];
+		// 3 3
+		// 1 2 3
+		// 4 5 6
+		// 7 8 9
+
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		dfs(0, 0);
+		System.out.println(sb);
+
+	}
+
+	static void dfs(int depth, int at) {
+		if (depth == m) {
+			for (int i = 0; i < m; i++) {
+				sb.append(answer[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		// 실제 알고리즘
+		for (int i = at; i < n; i++) {
+			answer[depth] = arr[i];
+			dfs(depth + 1, i);
+		}
+	}
+}
