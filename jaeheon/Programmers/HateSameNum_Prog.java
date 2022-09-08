bc23b8722dafc3cfa17104b21b1bfc0d132026cb
Jaeheon Jeong <drdd1120@naver.com>
2022-09-09 03:34:32 +0900

    20220909

--- jaeheon/Programmers/HateSameNum_Prog.java
+++ jaeheon/Programmers/HateSameNum_Prog.java
@@ -3,20 +3,41 @@ package Programmers;
 import java.util.*;
 
 public class HateSameNum_Prog {
+//	public int[] solution(int[] arr) {
+//
+//		ArrayList<Integer> list = new ArrayList<>();
+//		int tmp = 10;
+//		for (int i = 0; i < arr.length; i++) {
+//			if (arr[i] != tmp) {
+//				list.add(arr[i]);
+//				tmp = arr[i];
+//			}
+//		}
+//
+//		int[] answer = new int[list.size()];
+//		for (int i = 0; i < list.size(); i++) {
+//			answer[i] = list.get(i);
+//		}
+//
+//		return answer;
+//	}
 	public int[] solution(int[] arr) {
 
-		ArrayList<Integer> list = new ArrayList<>();
-		int tmp = 10;
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] != tmp) {
-				list.add(arr[i]);
-				tmp = arr[i];
+		Stack<Integer> stack = new Stack<>();
+		stack.push(arr[0]);
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] == stack.peek()) {
+				continue;
+			} else {
+				stack.push(arr[i]);
 			}
 		}
 
-		int[] answer = new int[list.size()];
-		for (int i = 0; i < list.size(); i++) {
-			answer[i] = list.get(i);
+		int[] answer = new int[stack.size()];
+
+		int idx = stack.size() - 1;
+		while (!stack.isEmpty()) {
+			answer[idx--] = stack.pop();
 		}
 
 		return answer;
